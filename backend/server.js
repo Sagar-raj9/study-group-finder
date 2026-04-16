1549652ff2fdf87e6a176c69f3a8a27ac65a8624
Sagar Raj <Sagar9097050@gmail.com>
2026-04-16 16:54:34 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ const { Server } = require("socket.io");
 
 const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 const app = express();
 
 app.use(express.json());
